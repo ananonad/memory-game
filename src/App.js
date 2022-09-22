a762ede391fe179005ad81b2bcff1c4a3c3f603c
annanadd <ananonadd@gmail.com>
2022-09-22 13:23:41 -0400

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { useEffect, useState, useRef } from "react";
 import Card from "./card";
-import imagesArray from './data';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  DialogTitle
+} from "@mui/material";
 import './app.scss';
 
 
@@ -37,33 +44,111 @@ const imagesArray = [
     type: "Smidge",
     image: require(`./images/Smidge.png`)
   },
-]
+];
 
 
-function swap(array, i, j) {
-  const temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
-}
 function shuffleCards(array) {
   const length = array.length;
   for(let i = length; i > 0; i--) {
     const randomIndex = Math.floor(Math.random() * i);
     const currentIndex = i - 1;
-    swap(array, currIndex, randomIndex)
+    const temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
   }
     return array;
 }
 
-export default function App( { imagesArray} ) {
+export default function App() {
   const [cards, setCards] = useState(
-    () => shuffleCards(imagesArray.concat(imagesArray))
+    shuffleCards.bind(null, imagesArray.concat(imagesArray))
   );
+    const [openCards, setOpenCards] = useState([]);
+    const [clearedCards, setClearedCards] = useState({});
+    const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
+    const [moves, setMoves] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+    const [bestScore, setBestScore] = useState(
+      JSON.parse(localStorage.getItem("bestScore")) || Number.POSITIVE_INFINITY
+    );
 
-  const handleClick = (index) => {
+    const timeout= useRef(null);
 
+    const disable = () => {
+      setShouldDisableAllCards(true);
+    };
+
+    const enable = () => {
+      setShouldDisableAllCards(false);
+    };
+
+    const checkCompletion = () => {
+      if(Object.keys(clearedCards).length === imagesArray.length) {
+        setShowModal(true);
+        const highScore = Math.min(moves, bestScore);
+        setBestScore(highScore);
+        localStorage.setItem("bestScore", highScore);
+      }
+    };
+
+    const evaluate = () => {
+    const [first, second] = openCards;
+    enable();
+    if(cards[first].type === cards[second].type) {
+      setClearedCards((prev) => ({...prev, [cards[first].type]: true }));
+      setOpenCards([]);
+      return;
+    }
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 300);
+  };
+
+
+  const handleCardClick = (index) => {
+    if(openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      setMoves((moves) => moves + 1);
+      disable();
+    } else {
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  useEffect(() => {
+    let timeout = null;
+  if(openCards.length === 2) {
+    timeout = setTimeout(evaluate, 300);
+  }
+  return () => {
+    clearTimeout(timeout);
+  };
+  }, [openCards]);
+
+
+  useEffect(() => {
+    checkCompletion();
+  }, [clearedCards]);
+
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card.type]);
   };
   
+
+  const handleRestart = () => {
+    setClearedCards({});
+    setOpenCards([]);
+    setShowModal(false);
+    setMoves(0);
+    setShouldDisableAllCards(false);
+    setCards(shuffleCards(imagesArray.concat(imagesArray)));
+  };
+
   return (
     <div className="App">
       <header>
@@ -79,13 +164,55 @@ export default function App( { imagesArray} ) {
               key={index}
               card={card}
               index={index}
+              isDisabled={shouldDisableAllCards}
+              isInactive={checkIsInactive(card)}
+              isFlipped={checkIsFlipped(index)}
               onClick={handleCardClick}
               />
           );
           })}
         </div>
+        <footer>
+          <div className="score">
+            <div className="moves">
+              <span className="bold">Moves:</span> {moves}
+            </div>
+            {localStorage.getItem(bestScore) && (
+              <div className="high-score">
+                <span className="bold">Best Score:</span> {bestScore}
+                </div>
+            )}
+          </div>
+          <div className="restart">
+            <Button onClick={handleRestart} color="primary" variant="contained">
+              Restart 
+            </Button>
+          </div>
+        </footer>
+        <Dialog
+          open={showModal}
+          disableBackdropClick
+          disableEscapeKeyDown
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            Hurray!!! You completed the challenge
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              You completed the game in {moves} moves. Your best score is{" "}
+              {bestScore} moves.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleRestart} color="primary">
+              Restart
+            </Button>
+          </DialogActions>
+        </Dialog>
     </div>
-  )
+  );
 }
 
-export default App;
+
